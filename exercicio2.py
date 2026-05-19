fb043871cb92840eca7b55bed9ffffb6bc288f53
Isabelassantos <isahsilva.0801@gmail.com>
2026-05-19 14:53:30 -0300

    update

--- exercicio2.py
+++ exercicio2.py
@@ -1,9 +1,9 @@
 #Exercício 2
-numeros = []
-
-for i in range(3):
-    numero = float(input("Digite um número: "))
-    numeros.append(numero)
-
-print("Maior número:", max(numeros))
-print("Menor número:", min(numeros))
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite um número: "))
+num3 = int(input("Digite um número: "))
+lista_num = [num1, num2, num3]
+maior_valor = max(lista_num)
+menor_valor = min(lista_num)
+print("O maior valor é {}".format(maior_valor))
+print("O menos valor é {}".format(menor_valor))
